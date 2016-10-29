b742c5fe171ea229e7b850e5fed239933d8c1250
Lennard <lennardschoemaker@gmail.com>
2016-10-29 20:12:53 +0200

    Create novalidate.js

---
+++ novalidate.js
@@ -0,0 +1,2 @@
+var buttonNoVal = document.getElementById("button");  // Get the button element with id="button"
+buttonNoVal.formNoValidate=true; //Add formNoValidate to disable native validation of form 
